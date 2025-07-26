0e34c8bbebd467f433dfb72207e5dacb955cd2e8
ck-surya <chhotu22@navgurukul.org>
2025-07-26 12:37:42 +0530

    added the domain name

--- lti-setup/index.js
+++ lti-setup/index.js
@@ -25,11 +25,11 @@ await lti.setup(
     devMode: true,
     dynRegRoute: "/register", // Setting up dynamic registration route. Defaults to '/register'
     dynReg: {
-      url: "http://localhost:3000/", // Tool Provider URL. Required field.
+      url: "https://lti.csbasics.in/", // Tool Provider URL. Required field.
       name: "quize", // Tool Provider name. Required field.
       logo: "https://imgs.search.brave.com/Nh8VoS-LeggCpHsK1WyrJ93y5ZzhdvOHID1hEXXjp6Y/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAyLzQ3LzAwLzYy/LzM2MF9GXzI0NzAw/NjIzMl9RS2hJNlUy/RlByNDlrUEJBZ09C/a2tvWWhOQXBxbG5W/Mi5qcGc", // Tool Provider logo URL.
       description: "Tool Description", // Tool Provider description.
-      redirectUris: ["http://localhost:3000/launch"], // Additional redirection URLs. The main URL is added by default.
+      redirectUris: ["https://lti.csbasics.in/launch"], // Additional redirection URLs. The main URL is added by default.
       //customParameters: { key: 'value' }, // Custom parameters.
       autoActivate: true, // Whether or not dynamically registered Platforms should be automatically activated. Defaults to false.
     },
